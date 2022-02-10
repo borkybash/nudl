a4b8c0a9d83f16b39e676920a2bb93a863764dc1
borkybash <borkybash@protonmail.com>
2022-02-09 23:50:39 -0500

    Add get_search_html function

--- pynud.py
+++ pynud.py
@@ -9,5 +9,12 @@ def get_search_url(args):
     search_url = "https://www.imdb.com/find?q=" + args
     return search_url
 
-print(get_search_url(sys.argv[1:]))
+def get_search_html(url):
+    res = requests.get(url)
+    res.raise_for_status()
+    search_html = bs4.BeautifulSoup(res.text, features='html.parser')
+    return search_html
+    
 
+print(get_search_url(sys.argv[1:]))
+search_url = get_search_url(sys.argv[1:])
