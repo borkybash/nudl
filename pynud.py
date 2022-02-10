ad2f3133c728a9231a66910770e44ad64aa70ca3
borkybash <borkybash@protonmail.com>
2022-02-09 23:17:35 -0500

    Update get_search_url function

--- pynud.py
+++ pynud.py
@@ -2,8 +2,12 @@
 # pynud: retrieve nudity info from movie database site
 
 import sys
+import requests, bs4
 
-args = "+".join(sys.argv[1:])
-search_query = "https://www.imdb.com/find?q=" + args
-print(search_query)
+def get_search_url(args):
+    args = "+".join(args)
+    search_url = "https://www.imdb.com/find?q=" + args
+    return search_url
+
+print(get_search_url(sys.argv[1:]))
 
